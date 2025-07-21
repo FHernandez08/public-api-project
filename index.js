207a75cfa1e1f5062ad0a346f908010dd2515fdf
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-07-21 15:48:50 -0500

    start of project

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const port = 5500;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// main route = this will be starting place of my app - it will use the index.ejs to show the webpage
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Listen on the port ${port}!`);
+    console.log(`The app is now LIVE! Please head to http://localhost:${port}`);
+})
